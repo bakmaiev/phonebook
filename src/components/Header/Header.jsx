447aaade78efdc7ecd836c4e5aff4a96a574f1eb
bakmaiev <thisyoungrhyme@gmail.com>
2023-06-26 18:43:20 +0300

    Add new check for Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,16 @@
 import AuthNav from 'components/AuthNav/AuthNav';
 import Navigation from 'components/Navigation/Navigation';
+import UserMenu from 'components/UserMenu/UserMenu';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectIsAuthorized } from 'redux/auth/selectors';
 
 const Header = () => {
+  const isAuthorizated = useSelector(selectIsAuthorized);
   return (
     <header>
-      <Navigation></Navigation>
-      <AuthNav></AuthNav>
+      <Navigation />
+      {isAuthorizated ? <UserMenu /> : <AuthNav />}
     </header>
   );
 };
