fcf220b4b619621f16ad5fb74ab3d080cf71132d
bakmaiev <thisyoungrhyme@gmail.com>
2023-06-28 12:32:55 +0300

    Add new changes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,25 +1,25 @@
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { lazy, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { refreshUser } from 'redux/auth/operations';
 import RestrictedRoute from 'components/RestrictedRoute/RestrictedRoute';
 import PrivateRoute from 'components/PrivateRoute/PrivateRoute';
-// import { selectIsRefreshing } from 'redux/auth/selectors';
-import Layout from 'components/Layout/Layout';
-import Home from 'pages/Home';
-import Contacts from 'pages/Contacts';
-import Login from 'pages/Login';
-import Register from 'pages/Register';
+import { selectIsRefreshing } from 'redux/auth/selectors';
+// import Layout from 'components/Layout/Layout';
+// import Home from 'pages/Home';
+// import Contacts from 'pages/Contacts';
+// import Login from 'pages/Login';
+// import Register from 'pages/Register';
 
-// const Layout = lazy(() => import('../Layout/Layout'));
-// const Home = lazy(() => import('../../pages/Home'));
-// const Login = lazy(() => import('../../pages/Login'));
-// const Register = lazy(() => import('../../pages/Register'));
-// const Contacts = lazy(() => import('../../pages/Contacts'));
+const Layout = lazy(() => import('../Layout/Layout'));
+const Home = lazy(() => import('../../pages/Home'));
+const Login = lazy(() => import('../../pages/Login'));
+const Register = lazy(() => import('../../pages/Register'));
+const Contacts = lazy(() => import('../../pages/Contacts'));
 
 const App = () => {
   const dispatch = useDispatch();
-  // const isRefreshing = useSelector(selectIsRefreshing);
+  const isRefreshing = useSelector(selectIsRefreshing);
 
   useEffect(() => {
     dispatch(refreshUser());
