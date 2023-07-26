cb6b3e098fe312105ca684c3ee945ac711f15c27
bakmaiev <thisyoungrhyme@gmail.com>
2023-07-27 02:40:52 +0300

    Add new changes

--- src/components/PhonebookForm/PhonebookForm.jsx
+++ src/components/PhonebookForm/PhonebookForm.jsx
@@ -17,8 +17,8 @@ import {
   createTheme,
 } from '@mui/material';
 import { ThemeProvider } from 'styled-components';
-import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 
 const schema = yup
